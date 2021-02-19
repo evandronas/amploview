d73676ecfe3b2760925ae3600140defbfddc0b20
Evandro Nascimento <evandronas@gmail.com>
2021-02-19 14:29:33 -0300

    Implementado configuração do banco h2, test profile e JPA

--- src/main/java/com/amploview/program/entities/Sites.java
+++ src/main/java/com/amploview/program/entities/Sites.java
@@ -2,10 +2,18 @@ package com.amploview.program.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Sites implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String Descricao;
 	
